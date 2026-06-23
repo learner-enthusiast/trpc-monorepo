924d38a89f9255cf32b2171037e75eb96237b8a7
arnb-smnta <loveumearnab.2812000@gmail.com>
2026-06-23 19:04:04 +0530

    feat: add .env.example file and update package.json scripts to use environment variables

--- packages/database/drizzle/0003_chemical_the_hand.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "forms" ALTER COLUMN "slug" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "forms" ALTER COLUMN "settings" SET DEFAULT '{"theme":"light","primaryColor":"#6C47FF","fontFamily":"DM Sans","showProgressBar":true,"shuffleQuestions":false,"allowMultipleSubmissions":false,"autoSaveDraft":true,"showSocialShare":false,"notifyOwnerOnSubmission":true,"thankYouMessage":"Thanks for your response! 🎉"}'::jsonb;--> statement-breakpoint
-ALTER TABLE "forms" ADD COLUMN "anonymous_responses" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "forms" ADD COLUMN "expiry" timestamp;--> statement-breakpoint
-ALTER TABLE "form_responses" ADD COLUMN "name" varchar(80);
--- packages/database/schema.ts
+++ packages/database/schema.ts
@@ -1,5 +1,1 @@
 export * from "./models/user";
-export * from "./models/form";
-export * from "./models/question";
-export * from "./models/formresponse";
-export * from "./models/answer";
